43d0e32ca859e2e6d21ceb8259aa31a4353cd331
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2023-01-01 18:56:00 +0600

    connect with mongoDB and insert data

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 4000;
 const app = express()
 
+require('dotenv').config()
 
 // middle ware 
 app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dptn3ue.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('travelWithSayed').collection('services')
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) => {
